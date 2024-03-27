3f388dd4dddc2e6528c729bed521fb1a7021c595
Robson Coura <robson_coura@yahoo.com.br>
2024-03-27 12:19:32 -0300

    Solução para 3 exercício

---
+++ DescubraALogica.java
@@ -0,0 +1,61 @@
+//3) Descubra a lógica e complete o próximo elemento:
+//
+//
+//
+//a) 1, 3, 5, 7, ___
+//
+//b) 2, 4, 8, 16, 32, 64, ____
+//
+//c) 0, 1, 4, 9, 16, 25, 36, ____
+//
+//d) 4, 16, 36, 64, ____
+//
+//e) 1, 1, 2, 3, 5, 8, ____
+//
+//f) 2,10, 12, 16, 17, 18, 19, ____
+
+public class DescubraALogica {
+
+    /*a) 1, 3, 5, 7, ___
+
+Observando os números, estão sendo adicionados números ímpares sequencialmente. O próximo número ímpar após 7 é 9. Então, o próximo elemento é 9.
+
+Resposta: 9
+
+b) 2, 4, 8, 16, 32, 64, ____
+
+Essa sequência é uma progressão geométrica, onde cada número é o dobro do anterior. Então, para encontrar o próximo elemento, basta dobrar o último número.
+
+64 * 2 = 128
+
+Resposta: 128
+
+c) 0, 1, 4, 9, 16, 25, 36, ____
+
+Essa sequência é o resultado da elevação ao quadrado dos números naturais começando de 0. Assim, o próximo número seria o quadrado de 7.
+
+7^2 = 49
+
+Resposta: 49
+
+d) 4, 16, 36, 64, ____
+
+Observando os números, estão sendo adicionados quadrados de números ímpares sequencialmente. O próximo número ímpar após 5 é 7, e seu quadrado é 49.
+
+Resposta: 49
+
+e) 1, 1, 2, 3, 5, 8, ____
+
+Esta sequência é a sequência de Fibonacci, onde cada número é a soma dos dois anteriores. O próximo número na sequência de Fibonacci após 8 é 13.
+
+Resposta: 13
+
+f) 2, 10, 12, 16, 17, 18, 19, ____
+
+Essa sequência não parece seguir uma regra óbvia. No entanto, parece que os números estão aumentando, mas não há uma regra clara para determinar o próximo número.
+
+Uma possível interpretação seria que cada número é aumentado por um valor que é maior que o anterior. Então, podemos preencher o próximo número como 20, pois 19 + 1 = 20.
+
+Resposta: 20
+*/
+}
